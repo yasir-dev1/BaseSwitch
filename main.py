eeb457ec1b12ca895750169271989b2291144203
yasir-dev1 <yasireposta3@gmail.com>
2024-06-09 02:32:04 +0300

    Update UI

--- main.py
+++ main.py
@@ -1,55 +1,52 @@
 import sys
 from PyQt6.uic import loadUi
-from PyQt6.QtWidgets import QApplication, QDialog
+from PyQt6.QtWidgets import QDialog,QApplication
+
 
 class App(QDialog):
     def __init__(self):
         super().__init__()
-        loadUi("GUI.ui", self)
+        loadUi("GUI.ui",self)
         self.bases = {
-            "Dec": 10,
-            "Hex": 16,
-            "Bin": 2,
-            "Oct": 8
+            "Dec":10,
+            "Hex":16,
+            "Oct":8,
+            "Bin":2,
         }
         self.number = None
-        self.from_base = self.bases[self.fromb.currentText()]
-        self.to_base = self.bases[self.to.currentText()]
-        self.result = None
+        self.result = ""
+        self.fromb = self.bases[self.from_base.currentText()]
+        self.tob = self.bases[self.to_base.currentText()]
 
-        self.fromb.currentIndexChanged.connect(self.onComboBoxChanged)
-        self.to.currentIndexChanged.connect(self.onComboBoxChanged)
-        self.convert.clicked.connect(self.convert_operation)
-        self.num.textChanged.connect(self.on_text_changed)
-
-    def on_text_changed(self, text):
-        self.number = text
+        self.from_base.currentIndexChanged.connect(self.onComboBoxChanged)
+        self.to_base.currentIndexChanged.connect(self.onComboBoxChanged)
+        self.Num.textChanged.connect(self.on_text_changed)
+        self.convert_btn.clicked.connect(self.convert_operation)
 
     def onComboBoxChanged(self):
-        self.from_base = self.bases[self.fromb.currentText()]
-        self.to_base = self.bases[self.to.currentText()]
-    
+        self.fromb = self.bases[self.from_base.currentText()]
+        self.tob = self.bases[self.to_base.currentText()]
+
+
+    def on_text_changed(self,text):
+        self.number = text
+
     def convert_operation(self):
-        if self.number is not None and self.from_base is not None and self.to_base is not None:
-            if not isinstance(self.number, str) or not isinstance(self.from_base, int) or not isinstance(
-                    self.to_base, int):
-                return "The input number should be a string, and bases should be integers."
-            if self.from_base < 2 or self.from_base > 16 or self.to_base < 2 or self.to_base > 16:
-                return "Bases should be between 2 and 16 inclusively."
-
+        if self.number is not None:
             try:
-                decimal_number = int(self.number, self.from_base)
+                decimal_number = int(self.number,self.fromb)
             except ValueError:
-                return "Invalid input. Please provide the input number in the correct format for the specified base."
-
-            self.result = ""
+                self.result = "Invaild input Please provide the input number in the correct form at for the specified base"
+            
             while decimal_number > 0:
-                remainder = decimal_number % self.to_base
+                remainder = decimal_number % self.tob
                 self.result = str(remainder) + self.result
-                decimal_number //= self.to_base
+                decimal_number //= self.tob
         else:
-            self.result = "Please Enter The Num and"
-        self.rs.setText(self.result)
+            self.result = "Please Enter The Data"
+        self.result_te.setPlainText(self.result)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
